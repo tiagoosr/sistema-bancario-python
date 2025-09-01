e492ceae8672d6f210cabd56a29135f6aaf37f78
Tiago Rodrigues <tiiago.santosr@gmail.com>
2025-09-01 09:56:11 -0300

    Update sistema_bancario.py
    transacoes diarias

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 menu = """
 ===== MENU =====
 
@@ -13,24 +15,42 @@ saldo = 0.0
 limite = 500.0
 extrato = ""
 numero_saques = 0
+transacoes_diarias = 10
+qtd_transacoes = 0
 LIMITE_SAQUES = 3
 
 
+def registrar_transacao(tipo, valor):
+    global extrato
+    agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    extrato += f"{agora} - {tipo}: R$ {valor:.2f}\n"
+
+
 def depositar(valor):
-    global saldo, extrato
+    global saldo, qtd_transacoes
+    
+    if qtd_transacoes >= transacoes_diarias:
+        print("Limite de transações diárias atingido.")
+        return
+    
     if valor > 0:
         saldo += valor
-        extrato += f"Depósito: R$ {valor:.2f}\n"
+        qtd_transacoes += 1
+        registrar_transacao("Depósito", valor)
         print(f"Depósito de R$ {valor:.2f} realizado com sucesso!")
     else:
         print("Valor inválido. O depósito deve ser positivo.")
 
 def sacar(valor):
-    global saldo, extrato, numero_saques
+    global saldo, numero_saques, qtd_transacoes
     excedeu_saldo = valor > saldo
     excedeu_limite = valor > limite
     excedeu_saques = numero_saques >= LIMITE_SAQUES
-
+    
+    if qtd_transacoes >= transacoes_diarias:
+        print("Você excedeu o número de transações diárias permitidas.")
+        return
+    
     if excedeu_saldo:
         print("Saldo insuficiente para realizar o saque.")
     elif excedeu_limite:
@@ -39,8 +59,9 @@ def sacar(valor):
         print("Você já atingiu o limite diário de saques (3).")
     elif valor > 0:
         saldo -= valor
-        extrato += f"Saque: R$ {valor:.2f}\n"
         numero_saques += 1
+        qtd_transacoes += 1
+        registrar_transacao("Saque", valor)
         print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
     else:
         print("Valor inválido. O saque deve ser positivo.")
@@ -48,7 +69,12 @@ def sacar(valor):
 
 def exibir_extrato():
     print("\n" + " EXTRATO ".center(40, "="))
-    print("Nenhuma movimentação realizada.." if not extrato else extrato)
+
+    if not extrato:
+        print("Nenhuma movimentação realizada..")
+    else:
+        print(extrato, end="") 
+
     print(f"\nSaldo: R$ {saldo:.2f}")
     print("=".center(40, "="))
 
